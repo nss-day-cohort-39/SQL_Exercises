80bf1a89d8d7186908486de1ad920f3e27a7f4e3
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2020-06-02 11:57:57 -0500

    group by

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -31,9 +31,56 @@
 
 --DELETE FROM Genre WHERE Label = 'Techno';
 
---SELECT Id, Label FROM Genre WHERE Label LIKE '%C';
+--SELECT [Id], [Label] FROM Genre WHERE Label LIKE '%C%';
 
-SELECT SongLength, Title FROM Song WHERE Id = 18;
+-- SELECT SongLength, Title FROM Song WHERE Id = 18;
 
-UPDATE Song
-SET SongLength = 515;
+--UPDATE Song
+--SET SongLength = 515;
+
+--SELECT Title FROM (
+--    SELECT
+--        song.Id,
+--        song.Title,
+--        song.ReleaseDate,
+--        song.GenreId,
+--        song.ArtistId,
+--        artist.ArtistName
+--    FROM Song song
+--    JOIN Artist artist ON song.ArtistId = artist.Id
+--) AS Title;
+
+--SELECT
+--        song.Id AS [Song Id],
+--        song.Title,
+--        song.ReleaseDate,
+--        song.GenreId,
+--        song.ArtistId,
+--        artist.ArtistName,
+--        artist.Id AS ArtistId
+--    FROM Song song
+--    JOIN Artist artist ON song.ArtistId = artist.Id
+
+
+--SELECT
+--    song.Id,
+--    song.Title,
+--    song.ReleaseDate,
+--    song.GenreId,
+--    song.ArtistId,
+--    artist.ArtistName
+--FROM Song song
+--JOIN Artist artist ON song.ArtistId = artist.Id;
+
+
+--SELECT artist.ArtistName, Count(song.Id) AS 'Song Count' --something for a count of songs
+--FROM Artist artist
+--JOIN Song song ON song.ArtistId = artist.Id
+--GROUP BY (artist.ArtistName)
+
+
+SELECT genre.Label, COUNT(song.Id) AS 'Song Count' 
+FROM Genre genre
+INNER JOIN Song song ON song.GenreId = genre.Id
+GROUP BY (genre.Label)
+ORDER BY COUNT(song.Id) DESC
